e2d328fb269592fb758d8d6f2567790f10abc460
Eric M. G. <ericbro29@gmail.com>
2024-09-25 16:24:00 +0300

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,43 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
+import purgecss from 'astro-purgecss';
+import { getPage } from './src/pocketbase.ts';
+// @ts-ignore
+const { expand: { site_details: { url, theme_color, title } } } = await getPage();
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: "static",
+    outDir: process.cwd() + '\\build\\' + url.slice(8),
+    site: url,
+
+    build: {
+        assets: 'assets',
+        inlineStylesheets: 'never'
+    },
+
+    image: {
+        domains: ["127.0.0.1"]
+    },
+
+    compressHTML: false,
+    integrations: [sitemap({
+        changefreq: 'weekly',
+        priority: 0.7,
+        lastmod: new Date(),
+        i18n: {
+            defaultLocale: 'en',
+            locales: {
+                en: 'en-US'
+            }
+        }
+    }), icon(), purgecss({
+        content: [
+            process.cwd() + '/src/**/*.astro'
+        ],
+        keyframes: true,
+        fontFace: true,
+        variables: true
+    })]
+});
---
+++ src/pocketbase.ts
@@ -0,0 +1,14 @@
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+const landingPageId = 'n4opl6kvtvrnc1v'
+
+const getImageUrl = ({ collection, filename }:{collection:any, filename:string}) => pb.files.getUrl(collection, filename)
+
+const getPage = async () => {
+  await pb.admins.authWithPassword('eric.gathoni@yellowpageskenya.com', 'CDz5pFLmm3thaFZ');
+  const record = await pb.collection("landing_page").getOne(landingPageId, { expand: 'style, site_details, hero, hero.image,hero.multiple_content, pages, pages.page_image, pages.card_grid, pages.card_grid.image, pages.multiple_content,pages.multiple_content.image,pages.multiple_content.card_grid,pages.multiple_content.card_grid.image' });
+  return record;
+}
+
+export { getImageUrl, getPage }
